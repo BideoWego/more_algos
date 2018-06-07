8df5c8b3d347045b52495104603364d929e677ab
Chris Scavello <bideowego@gmail.com>
2018-06-07 11:15:20 -0400

    Add ease the array algo

---
+++ ease_the_array.js
@@ -0,0 +1,37 @@
+/*
+https://practice.geeksforgeeks.org/problems/ease-the-array/0
+
+Input : arr[] = {2, 2, 0, 4, 0, 8}
+Output : 4 4 8 0 0 0
+
+Input : arr[] = {0, 2, 2, 2, 0, 6, 6, 0, 0, 8}
+Output :  4 2 12 8 0 0 0 0 0 0
+*/
+
+function ease(array) {
+  let numZeros = 0;
+  const eased = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const a = array[i];
+    const b = array[i + 1];
+    if (a === 0) {
+      numZeros++;
+    } else if (a === b) {
+      eased.push(a * 2);
+      array[i + 1] = 0;
+    } else {
+      eased.push(a);
+    }
+  }
+
+  while (numZeros) {
+    eased.push(0);
+    numZeros--;
+  }
+
+  return eased;
+}
+
+console.log(ease([2, 2, 0, 4, 0, 8]));
+console.log(ease([0, 2, 2, 2, 0, 6, 6, 0, 0, 8]));
